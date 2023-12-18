7fcf3fd71dd4142e5ac37d3ecd44a67b404bd8a1
s4brina <138499374+s4brina@users.noreply.github.com>
2023-12-18 17:22:18 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+// Toggle the active class for the burger menu icon and menu items
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+});
 
